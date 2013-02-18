95d6ad478d9fd39688242c8afb91415d73318271
Artur Opala <opala.artur@gmail.com>
2013-02-18 16:39:52 +0100

    crossings part 1

--- src/main/scala/org/opalaart/dots/DotsGame.scala
+++ src/main/scala/org/opalaart/dots/DotsGame.scala
@@ -1,154 +1,198 @@
 package org.opalaart.dots
 
 trait Color {
-  override def toString:String = super.toString.split('.').last.split('$').head+")"
+	override def toString: String = super.toString.split('.').last.split('$').head + ")"
 }
 
 object WHITE extends Color
+
 object BLACK extends Color
+
 object RED extends Color
+
 object BLUE extends Color
+
 object GRAY extends Color
 
-case class Dot(h:Int, w:Int, private val board:DotsBoard){
-  var color:Color = WHITE
-  
-  lazy val index = board.indexOf(h,w)
-  lazy val adjacent:Set[Dot] = board.adjacentOf(h,w) 
-  lazy val edges:Set[Edge] = board.adjacentOf(h,w) map (board.createEdge(this,_)) 
-  
-  def edgeTo(target:Dot):Option[Edge] = edges.find(edge => edge.target==target)
+case class Dot(h: Int, w: Int, private val board: DotsBoard) {
+	var color: Color = WHITE
 
-  def adjustEdges {edges foreach (_.adjust)}
-  
-  override def toString:String = "Dot("+w+","+h+","+color+")"
+	lazy val index = board.indexOf(h, w)
+	lazy val adjacent: Set[Dot] = board.adjacentOf(h, w)
+	lazy val edges: Set[Edge] = board.targetsOf(h, w) map (board.createEdge(this, _))
+
+	def vectorTo(target: Dot): Vector = (target.h-h,target.w-w)
+	def dotAtOffset(vector: Vector): Option[Dot] = board(h+vector._1,w+vector._2)
+	def edgeTo(target: Dot): Option[Edge] = edges.find(edge => edge.target == target)
+
+	def adjustEdges {edges foreach (_.adjust)}
+
+	override def toString: String = "Dot(" + w + "," + h + "," + color + ")"
 }
 
-case class Edge(source:Dot, target:Dot, var color:Color = WHITE, var taken:Boolean = false){
-  
-  def adjust {
-    if(color==WHITE){
-      if(source.color==target.color) {
-	      color = source.color
-      } else if(source.color!=WHITE && source.color!=BLACK && target.color!=WHITE && target.color!=BLACK){
-		  if(source.color!=target.color) color = GRAY
-	  }
-    }
-	adjustSymmetricEdge
-  }
-	
-  def adjustSymmetricEdge {
-	  val symmetricEdge = target.edgeTo(source).get
-	  symmetricEdge.color = color
-	  symmetricEdge.taken = taken
-  }
+case class Edge(source: Dot, target: Dot, var color: Color = WHITE, var taken: Boolean = false) {
+
+	def adjust {
+		if (color == WHITE || color==BLACK) {
+			if (source.color == target.color) {
+				color = source.color
+			} else if (source.color != WHITE && source.color != BLACK && target.color != WHITE && target.color != BLACK) {
+				if (source.color != target.color) color = GRAY
+			}
+		}
+		adjustSymmetricEdge
+	}
+
+	def adjustSymmetricEdge {
+		val symmetricEdge = target.edgeTo(source).get
+		symmetricEdge.color = color
+		symmetricEdge.taken = taken
+	}
 }
 
-class DotsBoard(height:Int, width:Int) extends Traversable[Dot]{
-  
-  private val dots = new Array[Array[Option[Dot]]](height)
-  private val edges = scala.collection.mutable.ArrayBuffer[Edge]()
-  
-  //board arrays initialization with None
-  for(h <- 0 until height){
-    dots(h) = new Array[Option[Dot]](width)
-    for(w <- 0 until width) dots(h)(w) = None
-  }
-  
-  def apply(h:Int,w:Int):Option[Dot] = {
-    if(h<0 || h>=height || w<0 || w>=width) None
-    else dots(h)(w) orElse {
-    	val dotOpt = Some(createDot(h,w))
-    	dots(h)(w) = dotOpt
-    	dotOpt
-    }
-  }
-  
-  def apply(p:(Int,Int)):Option[Dot] = apply(p._1,p._2)
-  
-  def apply(index:Int):Option[Dot] = {
-    if(index<0 || index>=size) None
-    else this(locate(index))
-  }
-  
-  def dot(h:Int,w:Int) = this(h,w) getOrElse {
-    throw new IllegalArgumentException("("+h+","+w+")")
-  }
-  
-  def indexOf(h:Int,w:Int) = h*width + w
-  def locate(index:Int):(Int,Int) = (index/width,index%width)
-  
-  def adjacentOf(h:Int,w:Int):Set[Dot] = DotsGameRules.moves map {
-    case (dh,dw) => this(h+dh,w+dw) 
-  } filter (_.isDefined) map (_.get)
-  
-  def createDot(h:Int,w:Int):Dot = Dot(h,w,this)
-  
-  def createEdge(source:Dot,target:Dot):Edge = {
-    val edge = Edge(source,target)
-    edges += edge
-    edge
-  }
-  
-  //extends trait Traversable
-  override val size = width*height
-  def foreach[U](f: Dot => U): Unit = {
-    for(h <- 0 until height; w <- 0 until width) dots(h)(w).map(f)
-  }
-  
+class DotsBoard(height: Int, width: Int) extends Traversable[Dot] {
+
+	private val dots = new Array[Array[Option[Dot]]](height)
+	private val edges = scala.collection.mutable.ArrayBuffer[Edge]()
+
+	//board arrays initialization with None
+	for (h <- 0 until height) {
+		dots(h) = new Array[Option[Dot]](width)
+		for (w <- 0 until width) dots(h)(w) = None
+	}
+
+	def apply(h: Int, w: Int): Option[Dot] = {
+		if (h < 0 || h >= height || w < 0 || w >= width) None
+		else dots(h)(w) orElse {
+			val dotOpt = Some(createDot(h, w))
+			dots(h)(w) = dotOpt
+			dotOpt
+		}
+	}
+
+	def apply(p: Vector): Option[Dot] = apply(p._1, p._2)
+
+	def apply(index: Int): Option[Dot] = {
+		if (index < 0 || index >= size) None
+		else this(locate(index))
+	}
+
+	def dot(h: Int, w: Int) = this(h, w) getOrElse {
+		throw new IllegalArgumentException("(" + h + "," + w + ")")
+	}
+
+	def indexOf(h: Int, w: Int) = h * width + w
+
+	def locate(index: Int): Vector = (index / width, index % width)
+
+	def adjacentOf(h: Int, w: Int): Set[Dot] = DotsGameRules.moves map {
+		case (dh, dw) => this(h + dh, w + dw)
+	} filter (_.isDefined) map (_.get)
+
+	def targetsOf(h: Int, w: Int): Set[Dot] = DotsGameRules.edges map {
+		case (dh, dw) => this(h + dh, w + dw)
+	} filter (_.isDefined) map (_.get)
+
+	def createDot(h: Int, w: Int): Dot = Dot(h, w, this)
+
+	def createEdge(source: Dot, target: Dot): Edge = {
+		val edge = Edge(source, target)
+		edges += edge
+		edge
+	}
+
+	//extends trait Traversable
+	override val size = width * height
+
+	def foreach[U](f: Dot => U): Unit = {
+		for (h <- 0 until height; w <- 0 until width) dots(h)(w).map(f)
+	}
+
 }
 
 object DotsGameRules {
-  
-  val moves:Set[(Int,Int)] = Set(
-      (-1,-1), (-2,0), (0,2), (2,-1), 
-      (-2,1), (2,0), (1,-2), (1,1), (0,-2), 
-      (1,-1), (-1,0), (-2,-1), (2,2), (-1,2), 
-      (0,1), (-2,2), (-1,1), (1,2), (-2,-2), 
-      (2,1), (1,0), (-1,-2), (0,-1), (2,-2)
-  )
+
+	val moves: Set[Vector] = Set(
+		(-1, -1), (-2, 0), (0, 2), (2, -1),
+		(-2, 1), (2, 0), (1, -2), (1, 1), (0, -2),
+		(1, -1), (-1, 0), (-2, -1), (2, 2), (-1, 2),
+		(0, 1), (-2, 2), (-1, 1), (1, 2), (-2, -2),
+		(2, 1), (1, 0), (-1, -2), (0, -1), (2, -2)
+	)
+
+	val edges: Set[Vector] = Set(
+		(-1, -1), (2, -1), (-1, 2), (-2, 1), (1, -2), (1, 1),
+		(1, -1), (-1, 0), (-2, -1), (0, 1), (-1, 1), (1, 2),
+		(2, 1), (1, 0), (-1, -2), (0, -1)
+	)
+
+	val doubles: Set[Vector] = Set(
+		(-2, 0), ( 0, 2), ( 2, 0), ( 0,-2), ( 2, 2),(-2, 2), (-2,-2), ( 2,-2)
+	)
+	
+	val crossings: Map[Vector,Set[(Vector,Vector)]] = Map(
+		(1,0) -> Set(((0,-1),(1,2)),((0,1),(1,-2))),
+		(-1,0) -> Set(((0,-1),(-1,2)),((0,1),(-1,-2))),
+		(0,1) -> Set(((-1,0),(2,1)),((1,0),(-2,1))),
+		(0,-1) -> Set(((-1,0),(2,-1)),((1,0),(-2,-1)))
+	)
 
 }
 
-class DotsGame(height:Int,width:Int) {
-  
-  val board = new DotsBoard(height,width)
-  
-  private var counter=0;
-  
-  def take(h:Int,w:Int,color:Color):Dot = {
-    val dot = board.dot(h,w)
-    assert(counter==0 || dot.color==BLACK, s"$dot should be BLACK")
-    counter = counter+1
-    dot.color = color
-    dot.adjacent filter (_.color==WHITE) foreach (_.color=BLACK)
-    dot.adjustEdges
-    dot
-  }
-  
-  def takeRed(h:Int,w:Int) = take(h,w,RED)
-  def takeBlue(h:Int,w:Int) = take(h,w,BLUE)
-  
-  def connect(h1:Int,w1:Int,h2:Int,w2:Int,color:Color):Edge = {
-    val source = board.dot(h1,w1)
-    assert(source.color==color, s"$source should be $color")
-    val target = board.dot(h2,w2)
-    assert(target.color==color, s"target should be $color")
-    connect(source,target,color)
-  }
-  
-  def connect(source:Dot,target:Dot,color:Color):Edge = {
-    val edge = source.edgeTo(target).getOrElse(
-        throw new IllegalArgumentException(s"$target not reachable from $source")
-    )
-    assert(edge.color==color, s"$edge should be $color")
-    //TODO check crossings
-    edge.taken = true
-	edge.adjustSymmetricEdge
-    edge
-  }
-  
-  def connectRed(source:Dot,target:Dot) = connect(source,target,RED)
-  def connectBlue(source:Dot,target:Dot) = connect(source,target,BLUE)
-  
+class DotsGame(height: Int, width: Int) {
+
+	val board = new DotsBoard(height, width)
+
+	private var counter = 0;
+
+	def take(h: Int, w: Int, color: Color): Dot = {
+		val dot = board.dot(h, w)
+		take(dot,color)
+	}
+
+	def take(dot: Dot, color: Color): Dot = {
+		assert(counter == 0 || dot.color == BLACK || dot.color==color, s"$dot should be BLACK or $color")
+		counter = counter + 1
+		dot.color = color
+		dot.adjacent filter (_.color == WHITE) foreach {dot =>
+			dot.color = BLACK
+			dot.adjustEdges
+		}
+		dot.adjustEdges
+		dot
+	}
+
+	def takeRed(h: Int, w: Int) = take(h, w, RED)
+	def takeBlue(h: Int, w: Int) = take(h, w, BLUE)
+
+	def connect(h1: Int, w1: Int, h2: Int, w2: Int, color: Color): Edge = {
+		val source = board.dot(h1, w1)
+		assert(source.color == color, s"$source should be $color")
+		val target = board.dot(h2, w2)
+		assert(target.color == color, s"$target should be $color")
+		connect(source, target, color)
+	}
+
+	def connect(source: Dot, target: Dot, color: Color): Edge = {
+		val vector = source.vectorTo(target)
+		if (DotsGameRules.doubles.contains(vector)){
+			val middle = source.dotAtOffset((vector._1/2,vector._2/2)).get
+			take(middle,color)
+			connect(middle,target,color)
+			connect(source,middle,color)
+		} else {
+			val edge = source.edgeTo(target).getOrElse(
+				throw new IllegalArgumentException(s"$target not reachable from $source")
+			)
+			assert(edge.color == color, s"$edge should be $color")
+			//TODO check crossings
+			edge.taken = true
+			edge.adjustSymmetricEdge
+			edge
+		}
+	}
+
+	def connectRed(source: Dot, target: Dot) = connect(source, target, RED)
+	def connectBlue(source: Dot, target: Dot) = connect(source, target, BLUE)
+
 }
--- src/test/scala/org/opalaart/dots/DotsGameTest.scala
+++ src/test/scala/org/opalaart/dots/DotsGameTest.scala
@@ -3,114 +3,123 @@ package org.opalaart.dots
 import org.scalatest.FunSpec
 
 class DotsGameTest extends FunSpec {
-  
-  describe("A Board") {
-    
-    it ("should have size equal to width*height") {
-      val board = new DotsBoard(40,30)
-      assert(board.size==30*40)
-    }
-    it ("should have unique index for all dots") {
-      val board = new DotsBoard(40,30)
-      assert(board.indexOf(0,0)==0)
-      assert(board.indexOf(10,10)==10*30+10)
-      assert(board.indexOf(39,29)==1199)
-    }
-    it ("should locate all dots by unique index") {
-      val board = new DotsBoard(40,30)
-      assert(board.locate(0)==(0,0))
-      assert(board.locate(10*30+10)==(10,10))
-      assert(board.locate(1199)==(39,29))
-    }
-    it ("should have all dots None at start") {
-      val board = new DotsBoard(40,30)
-      assert(board.count(dot => true)==0)
-    }
-    it ("should return adjacent of dot") {
-      val board = new DotsBoard(40,30)
-      val dot = board.dot(2,2)
-      val adjacent = dot.adjacent
-      adjacent.foreach(dot => assert(dot.color == WHITE))
-    }
-    it ("should apply return None for location outside board") {
-      val board = new DotsBoard(40,30)
-      val dot = board(-2,32)
-      assert(dot==None,dot)
-    }
-    it ("should apply return None for index outside board") {
-      val board = new DotsBoard(40,30)
-      val dot = board(-20)
-      assert(dot==None,dot)
-    }
-    it ("should dot throw Exception for index outside board") {
-      val board = new DotsBoard(40,30)
-      intercept[IllegalArgumentException]{
-    	  val dot = board.dot(-2,32)
-      }
-    }
-  }
-  
-  describe("A Dot") {
-    
-    it ("should have adjacent dots") {
-      val board = new DotsBoard(40,30)
-      val dot = board.dot(10,10)
-      assert(dot.adjacent.size==DotsGameRules.moves.size)
-    }
-    it ("should have traversable edges") {
-      val board = new DotsBoard(40,30)
-      val dot = board.dot(10,10)
-      assert(dot.edges.size==DotsGameRules.moves.size)
-    }
-    it ("should find traversable edge") {
-      val board = new DotsBoard(40,30)
-      val dot1 = board.dot(10,10)
-      val dot2 = board.dot(11,12)
-      val dot3 = board.dot(15,15)
-      assert(dot1.edgeTo(dot2).isDefined)
-      assert(dot1.edgeTo(dot3)==None)
-    }
-  }
-  
-  describe("A DotsGameRules") {
-    
-    it ("should have moves set of size 24") {
-      assert(DotsGameRules.moves.size==24)
-    }
-  }
-  
-  describe("A DotsGame") {
-    
-    it ("should take first dot with blue and set adjacent to black") {
-      val game = new DotsGame(40,30)
-      game.board.foreach(dot => assert(dot.color == WHITE))
-      game.takeBlue(2,2)
-      assert(game.board.dot(2,2).color==BLUE)
-      val adjacent = game.board.dot(2,2).adjacent
-      adjacent foreach (dot => assert(dot.color==BLACK,dot))
-    }
-    it ("should take next dot with red and set adjacent to black") {
-      val game = new DotsGame(40,30)
-      game.takeBlue(2,2)
-      assert(game.board.dot(2,2).color==BLUE)
-      val adjacent1 = game.board.dot(2,2).adjacent
-      adjacent1 foreach (dot => assert(dot.color==BLACK,dot))
-      game.takeRed(4,4)
-      assert(game.board.dot(2,2).color==BLUE)
-      assert(game.board.dot(4,4).color==RED)
-      val adjacent2 = game.board.dot(4,4).adjacent
-      adjacent2 foreach (dot => assert(dot.color==BLACK || dot.color==BLUE,dot))
-    }
-    it ("should connect two blue dots with blue edge") {
-      val game = new DotsGame(40,30)
-      val dot1 = game.takeBlue(2,2)
-      dot1.adjacent foreach (dot => assert(dot.color==BLACK,s"adjacent $dot should be BLACK"))
-      val dot2 = game.takeBlue(3,4)
-	  val edge1 = dot1.edgeTo(dot2).get
-	  assert(edge1.color==BLUE,s"$edge1 color should be BLUE")  
-      val edge2 = game.connectBlue(dot1, dot2)
-      assert(edge2.taken==true,s"$edge2 should be taken")
-    }
-  }
+
+	describe("A Board") {
+
+		it("should have size equal to width*height") {
+			val board = new DotsBoard(40, 30)
+			assert(board.size == 30 * 40)
+		}
+		it("should have unique index for all dots") {
+			val board = new DotsBoard(40, 30)
+			assert(board.indexOf(0, 0) == 0)
+			assert(board.indexOf(10, 10) == 10 * 30 + 10)
+			assert(board.indexOf(39, 29) == 1199)
+		}
+		it("should locate all dots by unique index") {
+			val board = new DotsBoard(40, 30)
+			assert(board.locate(0) ==(0, 0))
+			assert(board.locate(10 * 30 + 10) ==(10, 10))
+			assert(board.locate(1199) ==(39, 29))
+		}
+		it("should have all dots None at start") {
+			val board = new DotsBoard(40, 30)
+			assert(board.count(dot => true) == 0)
+		}
+		it("should return adjacent of dot") {
+			val board = new DotsBoard(40, 30)
+			val dot = board.dot(2, 2)
+			val adjacent = dot.adjacent
+			adjacent.foreach(dot => assert(dot.color == WHITE))
+		}
+		it("should apply return None for location outside board") {
+			val board = new DotsBoard(40, 30)
+			val dot = board(-2, 32)
+			assert(dot == None, dot)
+		}
+		it("should apply return None for index outside board") {
+			val board = new DotsBoard(40, 30)
+			val dot = board(-20)
+			assert(dot == None, dot)
+		}
+		it("should dot throw Exception for index outside board") {
+			val board = new DotsBoard(40, 30)
+			intercept[IllegalArgumentException] {
+				val dot = board.dot(-2, 32)
+			}
+		}
+	}
+
+	describe("A Dot") {
+
+		it("should have adjacent dots") {
+			val board = new DotsBoard(40, 30)
+			val dot = board.dot(10, 10)
+			assert(dot.adjacent.size == DotsGameRules.moves.size)
+		}
+		it("should have traversable edges") {
+			val board = new DotsBoard(40, 30)
+			val dot = board.dot(10, 10)
+			assert(dot.edges.size == DotsGameRules.edges.size)
+		}
+		it("should find traversable edge") {
+			val board = new DotsBoard(40, 30)
+			val dot1 = board.dot(10, 10)
+			val dot2 = board.dot(11, 12)
+			val dot3 = board.dot(15, 15)
+			assert(dot1.edgeTo(dot2).isDefined)
+			assert(dot1.edgeTo(dot3) == None)
+		}
+	}
+
+	describe("A DotsGameRules") {
+
+		it("should have moves set of size 24") {
+			assert(DotsGameRules.moves.size == 24)
+		}
+		it("should have edges set of size 16") {
+			assert(DotsGameRules.edges.size == 16)
+		}
+	}
+
+	describe("A DotsGame") {
+
+		it("should take first dot with blue and set adjacent to black") {
+			val game = new DotsGame(40, 30)
+			game.board.foreach(dot => assert(dot.color == WHITE))
+			game.takeBlue(2, 2)
+			assert(game.board.dot(2, 2).color == BLUE)
+			val adjacent = game.board.dot(2, 2).adjacent
+			adjacent foreach (dot => assert(dot.color == BLACK, dot))
+		}
+		it("should take next dot with red and set adjacent to black") {
+			val game = new DotsGame(40, 30)
+			game.takeBlue(2, 2)
+			assert(game.board.dot(2, 2).color == BLUE)
+			val adjacent1 = game.board.dot(2, 2).adjacent
+			adjacent1 foreach (dot => assert(dot.color == BLACK, dot))
+			game.takeRed(4, 4)
+			assert(game.board.dot(2, 2).color == BLUE)
+			assert(game.board.dot(4, 4).color == RED)
+			val adjacent2 = game.board.dot(4, 4).adjacent
+			adjacent2 foreach (dot => assert(dot.color == BLACK || dot.color == BLUE, dot))
+		}
+		it("should connect two blue dots with blue edge") {
+			val game = new DotsGame(40, 30)
+			val dot1 = game.takeBlue(2, 2)
+			assert(dot1.color==BLUE, s"$dot1 should be BLUE")
+			dot1.adjacent foreach (dot => assert(dot.color == BLACK, s"adjacent $dot should be BLACK"))
+			val dot2 = game.takeBlue(4, 4)
+			assert(dot2.color==BLUE, s"$dot2 should be BLUE")
+			val edge1 = game.connectBlue(dot1, dot2)
+			assert(edge1.taken == true, s"$edge1 should be taken")
+			assert(edge1.color == BLUE, s"$edge1 should be BLUE")
+			val dot3 = game.board.dot(3, 3)
+			assert(dot3.color==BLUE, s"$dot3 should be BLUE")
+			val edge2 = dot3.edgeTo(dot2).get
+			assert(edge2.taken == true, s"$edge2 should be taken")
+			assert(edge2.color == BLUE, s"$edge2 should be BLUE")
+		}
+	}
 
 }
